6e1c48aff87cbcc35f38b359588037c7188065f9
Ashish <97246854+6abc@users.noreply.github.com>
2024-01-19 20:10:03 +0530

    Update django_starter.sh

--- django_starter.sh
+++ django_starter.sh
@@ -3,8 +3,7 @@ source .env/bin/activate
 pip install Django
 django-admin startproject CORE
 cd CORE
-SYSTEM_IP=$(hostname -I | awk '{print $1}')
-sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = ['$SYSTEM_IP']/" CORE/settings.py
+sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = ['*']/" CORE/settings.py
 pip freeze > requirements.txt
 python manage.py makemigrations
 python manage.py migrate
