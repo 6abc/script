c4cfb0dc7457a01b926e0d71bde99f0f9a9bc31e
Ashish <97246854+6abc@users.noreply.github.com>
2024-01-19 20:50:23 +0530

    Update django_starter.sh

--- django_starter.sh
+++ django_starter.sh
@@ -5,6 +5,7 @@ django-admin startproject CORE
 cd CORE
 sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = ['*']/" CORE/settings.py
 pip freeze > requirements.txt
+echo "manage.py runserver 0.0.0.0:8000" > run.sh
 python manage.py makemigrations
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+bash run.sh
